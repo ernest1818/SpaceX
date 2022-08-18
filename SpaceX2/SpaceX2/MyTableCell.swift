b62be66527a8cb9898913f238720c859fefc61be
Ernest Avagovich <erni1937@gmail.com>
2022-08-18 11:34:43 +0300

    new project Space-X

---
+++ SpaceX2/SpaceX2/MyTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableCell.swift
+//  SpaceX2
+//
+//  Created by Ernest Avagovich on 18.08.2022.
+//
+
+import UIKit
+
+class MyTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
